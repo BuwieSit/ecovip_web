badfeb70fd3cc1c85a428761693571e0081cb063
BuwieSit <santosbuhawie28@gmail.com>
2025-05-17 22:07:38 +0800

    package divs

--- src/pages/Content/content.js
+++ src/pages/Content/content.js
@@ -9,6 +9,16 @@ function Content() {
         <div className='main-wrapper'>
             <div className='package-wrapper'>
                 <div className='img-wrapper'><img src={masungi} alt='Destination-picture' id='package-img'></img></div>
+                <div className='title-wrapper'>
+                    <h2 id='packName'>Nature Package</h2>
+                </div>
+                <div className='info-wrapper'>
+                    <p id='description'>This nature package rejuvenates the soul.</p>
+                    <div className='more-wrapper'>
+                        <p id='price'>$$$</p>
+                        <p id='moreInfo'>More Information</p>
+                    </div>
+                </div>
             </div>
             <div className='package-wrapper'>
 
